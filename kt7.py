468759cd9d595e6a93323711c07bc051d66f5a5d
Namratha <namrathanjjadhav@gmail.com>
2024-07-16 19:19:47 +0530

    project

---
+++ kt7.py
@@ -0,0 +1,50 @@
+#string 
+apple = "fruit"
+f = "fruit"
+print(f)
+
+subject = """
+hindi
+         social
+english
+"""
+print(subject)
+
+#index
+j = "namratha jadhav kadam"
+print(j[9])
+print(j[15])
+print(j[-3])
+print(j[0:21])
+print(j[9:15])
+print(j[0:9])
+print(j[0:12:3])
+print(j[:9])
+
+#slicing
+print(j[::-1])
+
+#mathematical operator 
+#concatenation
+
+Q = "Queen"
+K = "King"
+print(Q + K)
+
+
+
+Q = "Queen"
+K = "King"
+print(Q+" "+ K)
+
+#repetation
+print(Q*10)
+print(K*6)
+
+#lenth 
+print(len(Q))
+print(len(K))
+print(len(j))
+
+
+
